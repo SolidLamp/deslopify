87bb0e370aec9aba20444fc267fb8b5d269a8e67
SolidLamp <138074663+SolidLamp@users.noreply.github.com>
2026-03-29 18:45:28 +0100

    Cleanup code

--- src/borderify.js
+++ src/borderify.js
@@ -1,20 +1,10 @@
-// TODO: allow blocking by id or by class
+const blockedIDs = [
+    "askmiso-ask-query_1-0",
+    "AIOverlay",
+    "mosaic-provider-career-guide-scout-promo",
+];
 
-const blockedIDs = ["askmiso-ask-query_1-0", "AIOverlay", "mosaic-provider-career-guide-scout-promo"];
 const blockedClasses = ["ai-header-button", "assistantIcon"];
-/* yfMjpkSCQBUG-SuvKWC1Mg== t3G8h4IshJXWYCDYGqYhiA== */
-
-/* var blockList = ""
-for (var i = 0; i < blockedClasses.length; ++i) {
-    // console.log(blockedClasses[i])
-    blockList += blockedClasses[i]
-    blockList += " "
-}
-blockList = blockList.slice(0, -1);
-// console.log(blockList)
-*/
-
-// let blockList = blockedClasses.join(" ");
 
 let elements = [];
 for (let i = 0; i < blockedClasses.length; ++i) {
@@ -25,22 +15,17 @@ for (let i = 0; i < blockedClasses.length; ++i) {
     }
 }
 
-
-// let elements = document.getElementsByClassName(blockList);
-// elements = elements.concat();
-
-console.log("Deslopify: " + elements.length.toString() + " elements detected.")
+console.log("Deslopify: " + elements.length.toString() + " elements detected.");
 
 for (let i = 0; i < blockedIDs.length; ++i) {
     let newElement = document.getElementById(blockedIDs[i]);
     if (newElement) {
         elements[elements.length] = newElement;
     }
-
 }
 
-console.log("Deslopify: " + elements.length.toString() + " elements detected.")
-let len = elements.length
+console.log("Deslopify: " + elements.length.toString() + " elements detected.");
+let len = elements.length;
 
 for (let i = 0; i < len; ++i) {
     elements[i].style.display = "none";
@@ -48,4 +33,8 @@ for (let i = 0; i < len; ++i) {
 }
 
 console.log("Deslopify: deleted " + len.toString() + " elements.");
-// browser.action.setBadgeText({text: len.toString()});
+
+/**
+ * TODO: Create background script and set up counter of blocked elements
+ * browser.action.setBadgeText({text: len.toString()});
+ */
