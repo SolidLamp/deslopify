cd65435bc7c4a53afc27ecedb14d59751dee328c
SolidLamp <138074663+SolidLamp@users.noreply.github.com>
2026-05-05 00:51:12 +0100

    [Refactor] Convert to TypeScript

---
+++ src/background.ts
@@ -0,0 +1,161 @@
+/*  Deslopify background script: hides 'in-your-face AI' elements on websites.
+    Copyright (C) 2026 SolidLamp
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
+export {}
+
+const api = typeof browser !== "undefined" ? browser : chrome;
+
+console.log(
+    "Deslopify is provided under the GNU Affero General Public License version 3. This extension is provided without warranty. Please see http://www.gnu.org/licenses/ for more details.",
+);
+
+const blocklistURL = api.runtime.getURL("assets/blocklist.json");
+const response = await fetch(blocklistURL);
+const blocklistObject = await response.json();
+console.log(blocklistObject);
+
+interface Blocklist {
+    classes: string[];
+    IDs: string[];
+    otherIdentifiers: string[];
+    textContent: string[];
+}
+
+interface MessageSender {
+    documentId?: string;
+    documentLifecycle?: string;
+    frameId?: number;
+    id?: string;
+    origin?: string;
+    tab?: Object;
+    tlsChannelId?: string;
+    url?: string;
+    userScriptWorldId?: string;
+}
+
+/**
+ * Updates the count as displayed on the badge next to the extension icon.
+ *
+ * @param count - The number to overwrite the count on the badge with.
+ * @param tabID - the current ID of the tab.
+ * @param sendResponse - the function given by onMessage.
+ */
+function updateBadgeCounter(
+    count: number,
+    tabID: number,
+    sendResponse: (a: Object) => void,
+): void {
+    let message: string = count.toString();
+    console.log("Received message!");
+    if (message == "0") {
+        sendResponse({ message: "Did not update." });
+    } else {
+        api.action.setBadgeText({ text: message, tabId: tabID });
+        api.action.setBadgeBackgroundColor({ color: "grey" });
+        sendResponse({ message: "Succesfully updated." });
+    }
+}
+
+/**
+ * Gets the blocklist
+ *
+ * @param domain - the domain of the webpage to check with the full blocklist.
+ * @returns the blocklist for the given domain.
+ */
+function getBlocklist(domain: string): Blocklist {
+    const defaultBlocklist: Blocklist = {
+        classes: [],
+        IDs: [],
+        otherIdentifiers: [],
+        textContent: [],
+    };
+    if (typeof domain != "string") {
+        console.warn("Deslopify: Invalid domain");
+        return defaultBlocklist;
+    }
+    if (domain in blocklistObject) {
+        let blocklist = blocklistObject[domain];
+        console.log(`Deslopify: Domain found: ${domain}!`);
+        console.log(blocklist);
+        return blocklist;
+    } else {
+        console.log(`Deslopify: Domain has no data: ${domain}`);
+        return defaultBlocklist;
+    }
+}
+
+api.runtime.onMessage.addListener(
+    (
+        message: Object,
+        sender: MessageSender,
+        sendResponse: (a: Object) => void,
+    ) => {
+        if (!Object.hasOwn(sender, "tab")) {
+            sendResponse({ message: "Error with MessageSender" });
+        }
+        if (typeof sender.tab != "object") {
+            sendResponse({ message: "Error with MessageSender" });
+        }
+        if (Object.hasOwn(sender.tab, "id")) {
+            sendResponse({ message: "Error with MessageSender" });
+        }
+        const tabID = sender.tab.id;
+        if (typeof message == "number") {
+            updateBadgeCounter(message, tabID, sendResponse);
+        } else if (
+            typeof message == "object" &&
+            "message" in message &&
+            "data" in message &&
+            message.message == "getBlocklist"
+        ) {
+            const blocklist = getBlocklist(message.data);
+            sendResponse({ message: blocklist });
+        } else {
+            sendResponse({ message: "Failed to parse message." });
+        }
+    },
+);
+
+/* api.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+    if (changeInfo.status !== "complete") return;
+    try {
+        await api.scripting.executeScript({
+            target: {
+                tabId: tabId,
+            },
+            files: ["borderify.js"],
+        });
+        console.log("Successfully injected content script!");
+    } catch (e) {
+        console.error(`failed to execute script: ${e}`);
+    }
+    // const sending = api.tabs.sendMessage(tabId, "Run");
+});
+
+api.action.onClicked.addListener(async (tab) => {
+    try {
+        await api.scripting.executeScript({
+            target: {
+                tabId: tab.id,
+            },
+            files: ["borderify.js"],
+        });
+        console.log("Successfully injected content script!");
+    } catch (e) {
+        console.error(`failed to execute script: ${e}`);
+    }
+    // const sending = api.tabs.sendMessage(tabId, "Run");
+}); */
---
+++ src/deslopify.ts
@@ -0,0 +1,175 @@
+/*  Deslopify content script: hides 'in-your-face AI' elements on websites.
+    Copyright (C) 2026 SolidLamp
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
+export {}
+
+const api = typeof browser !== "undefined" ? browser : chrome;
+
+interface Blocklist {
+    classes: string[];
+    IDs: string[];
+    otherIdentifiers: string[];
+    textContent: string[];
+}
+
+console.log(
+    "Deslopify is provided under the GNU Affero General Public License version 3. This extension is provided without warranty. Please see http://www.gnu.org/licenses/ for more details.",
+);
+
+let hostname = window.location.hostname;
+let domain = hostname;
+if (hostname.substring(0, 4) == "www.") {
+    domain = hostname.slice(4);
+}
+
+/**
+ * Adds an array of Elements to an array of HTMLElements, with checks.
+ *
+ * @param elementsForRemoval - the array of HTMLElements for removal to add to.
+ * @param elementsToAdd - the array of Elements to add if they are HTMLElements.
+ */
+function addElements(
+    elementsForRemoval: HTMLElement[],
+    elementsToAdd: Element[],
+): void {
+    for (let element of elementsToAdd) {
+        if (element instanceof HTMLElement) {
+            elementsForRemoval.push(element);
+        }
+    }
+}
+
+/**
+ * Deletes elements in the DOM based on their identifiers.
+ *
+ * @param blockedClasses - the names of classes to remove from the DOM.
+ * @param blockedIDs - the names of IDs to remove from the DOM.
+ * @param blockedOtherIdentifiers - CSS other identifiers to remove from DOM.
+ * @param blockedTextContent - the names of classes to remove from the DOM.
+ */
+function blockElements(
+    blockedClasses: string[],
+    blockedIDs: string[],
+    blockedOtherIdentifiers: string[],
+    blockedTextContent: string[],
+): void {
+    let elements: HTMLElement[] = [];
+
+    // Detect elements by class
+    for (let i = 0; i < blockedClasses.length; ++i) {
+        let newElements = document.getElementsByClassName(blockedClasses[i]);
+        if (newElements) {
+            let newElementArray: Element[] = Array.from(newElements);
+            addElements(elements, newElementArray);
+        }
+    }
+
+    // Detect elements by id
+    for (let i = 0; i < blockedIDs.length; ++i) {
+        let newElement = document.getElementById(blockedIDs[i]);
+        if (newElement) {
+            elements[elements.length] = newElement;
+        }
+    }
+
+    // Detect elements by other identifiers
+    for (let i = 0; i < blockedOtherIdentifiers.length; ++i) {
+        let newElements = document.querySelectorAll(blockedOtherIdentifiers[i]);
+        if (newElements) {
+            let newElementArray: Element[] = Array.from(newElements);
+            addElements(elements, newElementArray);
+        }
+    }
+
+    // Detect elements by text content
+    const textTags = "a, p, span, div, button";
+    const textElements = Array.from(document.querySelectorAll(textTags));
+    const blockedTextElements = textElements.filter((element) =>
+        blockedTextContent.includes(element.textContent.trim()),
+    );
+    addElements(elements, blockedTextElements);
+
+    console.log(elements.length.toString() + " elements detected.");
+    let len: number = elements.length;
+
+    for (let i = 0; i < len; ++i) {
+        elements[i].style.display = "none";
+    }
+
+    console.log("Deslopify: Deleted " + len.toString() + " elements.");
+
+    const response = api.runtime.sendMessage(len);
+}
+
+/**
+ * Calls blockElements with a packed blocklist.
+ * 
+ * @param blocklist - The blocklist to unpack
+ */
+function unpackBlocklist(blocklist: Blocklist): void {
+    let blockedClasses =
+        typeof blocklist.classes !== "undefined" ? blocklist.classes : [];
+    let blockedIDs =
+        typeof blocklist.classes !== "undefined" ? blocklist.classes : [];
+    let blockedOtherIdentifiers =
+        typeof blocklist.classes !== "undefined" ? blocklist.classes : [];
+    let blockedTextContent =
+        typeof blocklist.classes !== "undefined" ? blocklist.classes : [];
+    blockElements(
+        blockedClasses,
+        blockedIDs,
+        blockedOtherIdentifiers,
+        blockedTextContent,
+    );
+}
+
+// We need this loop if the user adds the extension while already on a page.
+let noConnection: boolean = true;
+let message: { message: string | Blocklist } = { message: "Connection error." };
+while (noConnection) {
+    try {
+        message = await api.runtime.sendMessage({
+            message: "getBlocklist",
+            data: domain,
+        });
+        noConnection = false;
+    } catch (e) {
+        noConnection = true;
+    }
+}
+
+let blocklist: Blocklist;
+if (typeof message.message === "object") {
+    blocklist = message.message;
+} else {
+    blocklist = {
+        classes: [],
+        IDs: [],
+        otherIdentifiers: [],
+        textContent: [],
+    };
+}
+
+unpackBlocklist(blocklist);
+
+const observer = new MutationObserver(() => {
+    unpackBlocklist(blocklist);
+});
+
+observer.observe(document.body, {
+    childList: true,
+    subtree: true,
+});
