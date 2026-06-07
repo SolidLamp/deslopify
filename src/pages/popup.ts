d065bc9b192da8462bbf7694d0c0479861ec6f15
SolidLamp <138074663+SolidLamp@users.noreply.github.com>
2026-06-07 20:29:28 +0100

    [Enhancement] Update popup UI dynamically for better UX

--- src/pages/popup.ts
+++ src/pages/popup.ts
@@ -7,49 +7,59 @@ interface StorageArea {
     set: (items: Record<string, unknown>) => Promise<void>;
 }
 
+async function getCurrentTab(): Promise<{
+    currentWebpage: object;
+    activeWebsite: string;
+}> {
+    // Get the current tab.
+    const activeTabs: object[] = await api.tabs.query({
+        active: true,
+        currentWindow: true,
+    });
+    const currentWebpage: object = activeTabs[0];
+
+    // Get url of current tab
+    let activeWebsite: string = "";
+    if ("url" in currentWebpage) {
+        activeWebsite = new URL(currentWebpage.url).hostname;
+    }
+    if (activeWebsite.substring(0, 4) == "www.") {
+        activeWebsite = activeWebsite.slice(4);
+    }
+    const returnValue = {
+        currentWebpage: currentWebpage,
+        activeWebsite: activeWebsite,
+    };
+    return returnValue;
+}
+
+async function getStoredArray(
+    id: string,
+    storageArea: StorageArea,
+): Promise<string[]> {
+    let storageArray: string[] = [];
+    let storage: object | string[] = await storageArea.get(id);
+
+    if (typeof storage === "object" && Object.hasOwn(storage, id)) {
+        storageArray = storage[id];
+    } else if (typeof storage === "object" && storage.constructor === Array) {
+        storageArray = storage;
+    }
+    return storageArray;
+}
+
 function storeOnPress(id: string, storageArea: StorageArea): void {
     const element: HTMLElement | null = document.getElementById(id);
 
     if (element != null) {
         element.addEventListener("click", async () => {
+            // Get current tab
+            const currentTab = await getCurrentTab();
+            const activeWebsite = currentTab.activeWebsite;
+            const currentWebpage = currentTab.currentWebpage;
+
             // Get the current array from memory
-            let storageArray: string[] = [];
-            let storage: object | string[] = await storageArea.get(id);
-
-            if (typeof storage === "object" && Object.hasOwn(storage, id)) {
-                storageArray = storage[id];
-            } else if (
-                typeof storage === "object" &&
-                storage.constructor === Array
-            ) {
-                storageArray = storage;
-            }
-
-            // Get the current tab.
-            const activeTabs: object[] = await api.tabs.query({
-                active: true,
-                currentWindow: true,
-            });
-            const currentWebpage: object = activeTabs[0];
-
-            /*.then(
-                    (value: object[]) => {
-                        currentWebpage = value[0];
-                    },
-                    (error: any) => {
-                        console.error(error);
-                        return;
-                    },
-                ); */
-
-            // Get url of current tab
-            let activeWebsite: string = "";
-            if ("url" in currentWebpage) {
-                activeWebsite = new URL(currentWebpage.url).hostname;
-            }
-            if (activeWebsite.substring(0, 4) == "www.") {
-                activeWebsite = activeWebsite.slice(4);
-            }
+            const storageArray = await getStoredArray(id, storageArea);
 
             // Modify array
             console.log(storageArray);
@@ -73,13 +83,99 @@ function storeOnPress(id: string, storageArea: StorageArea): void {
                 },
             );
 
+            // Update popup to reflect changes
+            updateDOM();
+
             // Reload tab
             const tabid: number = currentWebpage.id;
             api.tabs.reload(tabid);
-
         });
     }
 }
 
+async function updateDOM(): Promise<void> {
+    console.log("I am here!");
+
+    const currentTab = await getCurrentTab();
+    const activeWebsite = currentTab.activeWebsite;
+
+    const blocklistURL = api.runtime.getURL("assets/blocklist.json");
+    const response = await fetch(blocklistURL);
+    const blocklistObject: object = await response.json();
+    const supported: boolean = activeWebsite in blocklistObject;
+    let active: boolean = activeWebsite in blocklistObject;
+
+    const localArray = await getStoredArray(
+        "button-perm-allow",
+        api.storage.local,
+    );
+    const sessionArray = await getStoredArray(
+        "button-temp-allow",
+        api.storage.session,
+    );
+
+    console.log(localArray);
+    console.log(sessionArray);
+
+    if (
+        localArray.includes(activeWebsite) ||
+        sessionArray.includes(activeWebsite)
+    ) {
+        active = false;
+    }
+
+    const activeUI = document.getElementById("active");
+    const inactiveUI = document.getElementById("inactive");
+    const tempAllow = document.getElementById("button-temp-allow");
+    const permAllow = document.getElementById("button-perm-allow");
+    const urlDisplay = document.getElementById("current-webpage-url");
+
+    // Need to assert that all exist
+    if (!activeUI) throw new Error("#active missing");
+    if (!inactiveUI) throw new Error("#inactive missing");
+    if (!tempAllow) throw new Error("#button-temp-allow missing");
+    if (!permAllow) throw new Error("#button-perm-allow missing");
+    if (!urlDisplay) throw new Error("#current-webpage-url missing");
+
+    // Here we do the modification
+    if (active) {
+        activeUI.style.display = "block";
+        inactiveUI.style.display = "none";
+    } else {
+        activeUI.style.display = "none";
+        inactiveUI.style.display = "block";
+    }
+
+    // Check for buttons
+    if (localArray.includes(activeWebsite)) {
+        permAllow.textContent = "Enable blocking on this website permanently";
+    } else {
+        permAllow.textContent = "Disable blocking on this website permanently";
+    }
+    if (sessionArray.includes(activeWebsite)) {
+        tempAllow.textContent =
+            "Enable blocking on this website for this session";
+    } else {
+        tempAllow.textContent =
+            "Disable blocking on this website for this session";
+    }
+
+    // Unsupported websites
+    if (!supported) {
+        const buttons: HTMLElement[] = Array.from(
+            document.getElementsByClassName("supported"),
+        );
+        for (const element of buttons) {
+            element.style.display = "none";
+        }
+        activeUI.textContent = "Unsupported Website";
+        inactiveUI.textContent = "Unsupported Website";
+    }
+
+    // Update URL
+    urlDisplay.textContent = activeWebsite;
+}
+
 storeOnPress("button-temp-allow", api.storage.session);
 storeOnPress("button-perm-allow", api.storage.local);
+updateDOM();
